85cbbbfc2f9f1f646aad3e14b52d69a9f3f3a832
raihun <raihun@raihusoft.jp>
2017-11-11 06:07:02 +0900

    Add Networkクラス

---
+++ Network.py
@@ -0,0 +1,31 @@
+# -*- encoding:utf-8 -*-
+from Lora import Lora
+from threading import Thread
+
+
+class Network:
+    """
+        ネットワークの基礎クラス
+        ACK管理、TTL減算、
+    """
+    def __init__(self):
+        self.lora = Lora()
+        self.lora.addRecvlistener(self.recvEvent)
+
+        self.thSend = Thread(
+            target=self.sendThread,
+            args=(self.lora,)
+        )
+        self.thSend.setDaemon(True)
+        self.thSend.start()
+        return
+
+    def sendThread(self, lora):
+        while True:
+            cmd = input()
+            lora.send(cmd)
+        return
+
+    def recvEvent(self, msg):
+        print(msg)
+        return
