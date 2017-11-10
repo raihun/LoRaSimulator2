d7df7b1609d49a4fb229c034ddeb1dce6f041462
raihun <raihun@raihusoft.jp>
2017-11-11 05:26:17 +0900

    Modify ログの生成形式

--- Log.py
+++ Log.py
@@ -1,12 +1,14 @@
 # -*- encoding:utf-8 -*-
 from datetime import datetime
-from os.path import exists
+from os import mkdir
+from os.path import isdir
 
 
 class Log:
-    __filename = "log.txt"
+    __dirname = "logs"
 
     def __init__(self):
+        self.__checkDirectory()
         return
 
     @staticmethod
@@ -16,7 +18,14 @@ class Log:
         line = line + "[{0}]\t{1}\n".format(caption, message)
 
         # ファイル追記
-        with open(Log.__filename, "a") as fp:
+        path = Log.__dirname + "/" + datetime.now().strftime("Log_%Y%m%d.txt")
+        with open(path, "a") as fp:
             fp.write(line)
             fp.close()
+        return
+
+    def __checkDirectory(self):
+        if(not isdir(Log.__dirname)):
+            # ディレクトリ作成
+            mkdir(Log.__dirname)
         return
