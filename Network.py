7715d1617f683e4140ac22123948f701795fb0ad
raihun <raihun@raihusoft.jp>
2017-11-11 16:18:11 +0900

    Add パケット生成テストコード

--- Network.py
+++ Network.py
@@ -2,6 +2,7 @@
 from Lora import Lora
 from Packet import Packet
 from threading import Thread
+from time import sleep
 
 
 class Network:
@@ -12,10 +13,32 @@ class Network:
     def __init__(self):
         self.lora = Lora()
         self.lora.addRecvlistener(self.recvEvent)
+        self.recvPacket = Packet()
 
-        self.packet = Packet()
+        self.thSend = Thread(
+            target=self.sendThread,
+            args=(self.lora,)
+        )
+        self.thSend.setDaemon(True)
+        self.thSend.start()
+        return
+
+    def sendThread(self, lora):
+        while True:
+            p = Packet()
+            p.setPanId("0001")
+            p.setDatalinkDst("0002")
+            p.setDatalinkSrc("0001")
+            p.setNetworkDst("0002")
+            p.setNetworkSrc("0001")
+            p.setPacketType(7)
+            p.setTTL(31)
+            p.setSequenceNo(255)
+            p.setPayload("HELLO")
+            lora.send(p.exportPacket())
+            sleep(5.0)
         return
 
     def recvEvent(self, msg):
-        self.packet.importPacket(msg)
+        self.recvPacket.importPacket(msg)
         return
