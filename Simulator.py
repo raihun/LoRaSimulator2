20bf64f81c609e5ae65f373a217c4a792260d49c
raihun <raihun@raihusoft.jp>
2017-11-15 00:10:26 +0900

    Modify Simulator(最低限必要なメソッド列挙)

--- Simulator.py
+++ Simulator.py
@@ -1,9 +1,22 @@
 # -*- encoding:utf-8 -*-
 
 
-class Simulator:
+class Serial:
     """
         このクラスでは、ES920LRのシミュレータを作成する
     """
-    def __init__(self):
+    def __init__(self, devicename, baudrate):
+        self.flg = 0
         return
+
+    def write(self, data):
+        print("[Sim in] {0}".format(data))
+        self.flg = 1
+        return
+
+    def inWaiting(self):
+        return self.flg
+
+    def readline(self):
+        self.flg = 0
+        return "OK".encode()
