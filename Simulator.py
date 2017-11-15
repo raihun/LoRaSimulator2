635912f0338aee4ed8e0d0491f9cd34eea5cc425
raihun <raihun@raihusoft.jp>
2017-11-15 23:24:23 +0900

    Add TCPクライアント機能

--- Simulator.py
+++ Simulator.py
@@ -1,4 +1,7 @@
 # -*- encoding:utf-8 -*-
+from Config import Config
+import socket
+import sys
 
 
 class Simulator:
@@ -6,17 +9,75 @@ class Simulator:
         このクラスでは、ES920LRのシミュレータを作成する
     """
     def __init__(self, devicename, baudrate):
-        self.flg = 0
+        self.__config = Config()
+        self.__isStart = False
         return
 
-    def write(self, data):
-        print("[Sim in] {0}".format(data))
-        self.flg = 1
+    def write(self, rawdata):
+        data = rawdata.decode('utf-8').strip()
+        if(not self.__isStart):
+            self.__modeConfig(data)
+        else:
+            self.__modeStart(data)
         return
 
     def inWaiting(self):
-        return self.flg
+        if(not self.__isStart):
+            return 0
+        try:
+            self.recvmsg = self.sock.recv(4096).decode().strip()
+        except ConnectionResetError:  # サーバ側からの切断
+            self.sock.close()
+        except OSError:  # サーバダウン
+            print("[Error] サーバダウン")
+            self.sock.close()
+            sys.exit()
+
+        if(len(self.recvmsg) <= 0):
+            self.sock.close()
+        return len(self.recvmsg)
 
     def readline(self):
-        self.flg = 0
-        return "OK".encode()
+        return self.recvmsg.encode()
+
+    def __modeConfig(self, data):
+        # Split command
+        data = data.split(" ")
+        c = data[0]
+        if(len(data) > 1):
+            m = data[1]
+
+        # Set parameters
+        if(c is "b"):  # bw
+            self.__bw = int(m)
+        if(c is "c"):  # sf
+            self.__sf = int(m)
+        if(c is "d"):  # channel
+            self.__channel = int(m)
+        if(c is "e"):  # panid
+            self.__panid = str(m)
+        if(c is "f"):  # ownid
+            self.__ownid = str(m)
+        if(c is "z"):
+            self.__isStart = True
+            self.__connect()
+        return
+
+    def __modeStart(self, data):
+        print("[Run] {0}".format(data))
+        self.sock.send(data.encode())
+        return
+
+    def __connect(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect(
+                (self.__config.getHost(), int(self.__config.getPort()))
+            )
+            params = "{0:02X}{1:02X}{2:02X}{3}{4}".format(
+                self.__bw, self.__sf, self.__channel, self.__panid, self.__ownid
+            )
+            self.sock.send(params.encode())
+        except ConnectionRefusedError:  # 接続失敗
+            print("[Error] 接続失敗")
+        return
