137106589dcdc5d90579a95a55c29c8c49a622b0
raihun <raihun@raihusoft.jp>
2017-12-03 13:40:27 +0900

    Fix Configの遅延ロードによる他ノードの干渉問題

--- Config.py
+++ Config.py
@@ -8,121 +8,129 @@ class Config:
     __filename = "config.ini"
 
     def __init__(self):
-        self.__config = ConfigParser()
         self.__readConfig()
         return
 
     def getSimulator(self):
-        simulator = self.__config["General"]["Simulator"]
+        simulator = Config.__config["General"]["Simulator"]
         if(simulator == ""):
             simulator = "false"
         return simulator
 
     def getDevicename(self):
-        devicename = self.__config["Device"]["Name"]
+        devicename = Config.__config["Device"]["Name"]
         if(devicename == ""):
             devicename = "/dev/tty.USB0"
         return devicename
 
     def getBaudrate(self):
-        baudrate = self.__config["Device"]["Baudrate"]
+        baudrate = Config.__config["Device"]["Baudrate"]
         if(baudrate == ""):
             baudrate = "9600"
         return baudrate
 
     def getBandwidth(self):
-        bandwidth = self.__config["LoRa"]["Bandwidth"]
+        bandwidth = Config.__config["LoRa"]["Bandwidth"]
         if(bandwidth == ""):
             bandwidth = "4"  # 3, 4, 5, 6
         return bandwidth
 
     def getSpreadingfactor(self):
-        spreadingfactor = self.__config["LoRa"]["Spreadingfactor"]
+        spreadingfactor = Config.__config["LoRa"]["Spreadingfactor"]
         if(spreadingfactor == ""):
             spreadingfactor = "7"  # 7-12
         return spreadingfactor
 
     def getChannel(self):
-        channel = self.__config["LoRa"]["Channel"]
+        channel = Config.__config["LoRa"]["Channel"]
         if(channel == ""):
             channel = "1"
         return channel
 
     def getPanid(self):
-        panid = self.__config["LoRa"]["Panid"]
+        panid = Config.__config["LoRa"]["Panid"]
         if(panid == ""):
             panid = "0001"
         return panid
 
     def setOwnid(self, param):
-        self.__config["LoRa"]["Ownid"] = param
-        self.__writeConfig()
+        Config.__config["LoRa"]["Ownid"] = param
         return
 
     def getOwnid(self):
-        ownid = self.__config["LoRa"]["Ownid"]
+        ownid = Config.__config["LoRa"]["Ownid"]
         if(ownid == ""):
             ownid = "0001"
         return ownid
 
     def getPower(self):
-        power = self.__config["LoRa"]["Power"]
+        power = Config.__config["LoRa"]["Power"]
         if(power == ""):
             power = "13"
         return power
 
     def getHost(self):
-        host = self.__config["Simulator"]["Host"]
+        host = Config.__config["Simulator"]["Host"]
         if(host == ""):
             host = "127.0.0.1"
         return host
 
     def getPort(self):
-        host = self.__config["Simulator"]["Port"]
+        host = Config.__config["Simulator"]["Port"]
         if(host == ""):
             host = "25561"
         return host
 
     def getSimulatorDebug(self):
-        debug = self.__config["Simulator"]["Debug"]
+        debug = Config.__config["Simulator"]["Debug"]
         if(debug == ""):
             debug = "false"
         return debug
 
     """ コンフィグファイル 読込 """
     def __readConfig(self):
+        # 初回インスタンス化チェック
+        try:
+            if(Config.__isFisrtread):
+                return
+        except AttributeError:
+            Config.__isFisrtread = True
+
+        # 設定読み込み
+        Config.__config = ConfigParser()
+
         if(not exists(self.__filename)):
             # General
-            self.__config["General"] = {}
-            self.__config["General"]["Simulator"] = "false"
+            Config.__config["General"] = {}
+            Config.__config["General"]["Simulator"] = "false"
 
             # Device setting
-            self.__config["Device"] = {}
-            self.__config["Device"]["Name"] = "/dev/tty.USB0"
-            self.__config["Device"]["Baudrate"] = "9600"
+            Config.__config["Device"] = {}
+            Config.__config["Device"]["Name"] = "/dev/tty.USB0"
+            Config.__config["Device"]["Baudrate"] = "9600"
 
             # LoRa setting
-            self.__config["LoRa"] = {}
-            self.__config["LoRa"]["Bandwidth"] = "4"  # 3, 4:125kHz, 5, 6
-            self.__config["LoRa"]["Spreadingfactor"] = "7"  # 7-12
-            self.__config["LoRa"]["Channel"] = "1"
-            self.__config["LoRa"]["Panid"] = "0001"
-            self.__config["LoRa"]["Ownid"] = "0001"
-            self.__config["LoRa"]["Power"] = "13"  # Min:-4 to Max:13
+            Config.__config["LoRa"] = {}
+            Config.__config["LoRa"]["Bandwidth"] = "4"  # 3, 4:125kHz, 5, 6
+            Config.__config["LoRa"]["Spreadingfactor"] = "7"  # 7-12
+            Config.__config["LoRa"]["Channel"] = "1"
+            Config.__config["LoRa"]["Panid"] = "0001"
+            Config.__config["LoRa"]["Ownid"] = "0001"
+            Config.__config["LoRa"]["Power"] = "13"  # Min:-4 to Max:13
 
             # Simulator setting
-            self.__config["Simulator"] = {}
-            self.__config["Simulator"]["Host"] = "127.0.0.1"
-            self.__config["Simulator"]["Port"] = "25561"
-            self.__config["Simulator"]["Debug"] = "false"
+            Config.__config["Simulator"] = {}
+            Config.__config["Simulator"]["Host"] = "127.0.0.1"
+            Config.__config["Simulator"]["Port"] = "25561"
+            Config.__config["Simulator"]["Debug"] = "false"
 
             # Create config file
             self.__writeConfig()
-        self.__config.read(self.__filename)
+        Config.__config.read(self.__filename)
         return
 
     """ コンフィグファイル 書出 """
     def __writeConfig(self):
         with open(self.__filename, "w") as fp:
-            self.__config.write(fp)
+            Config.__config.write(fp)
         return
